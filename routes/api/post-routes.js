4f679ff5eba8a64a4792a1fb261de98081875e9b
Happy Gilmore <maryleone@rocketcentral.com>
2022-07-05 08:39:32 -0400

    comment and vote routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,40 +1,64 @@
 const router = require('express').Router();
-const { Post, User }=require('../../models');
+const { Post, User, Vote, Comment } =require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req,res) =>{
     console.log('======================');
-    Post.findAll ({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
-        
-        include: [
-            {
-                model:User,
-                attributes: ['username']
-            }
-        ]
-        
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      order: [['created_at', 'DESC']],
+      include: [
+        // include the Comment model here:
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+     })
+     .then(dbPostData => res.json(dbPostData))
+     .catch(err => {
+       console.log(err);
+       res.status(500).json(err);
+     });
     });
-});
 
 router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
-      order: [['created_at', 'DESC']], 
-      include: [
-        {
+      attributes: ['id', 'post_url', 'title', 'created_at'
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
     })
       .then(dbPostData => {
         if (!dbPostData) {
@@ -62,6 +86,17 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  // PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { userInfo } = require('os');
-const { User } = require ('../../models');
+const { User, Post, Vote, Comment } = require ('../../models');
 
 
 //GET /api/users
@@ -20,22 +20,43 @@ router.get('/', (req,res)=> {
 //GET /api/users/1
 router.get('/:id', (req,res) => {
     User.findOne({
-        attributes: {exclude: ['password']},
+        attributes: { exclude: ['password'] },
         where: {
-            id:req.params.id
-        }
-    })
-    .then(dbUserData=> {
-        if(!dbUserData) {
-            res.status(404).json({message: 'No user found with this id'});
-            return;      
+          id: req.params.id
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'post_url', 'created_at']
+          },
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          },
+          {
+            model: Post,
+            attributes: ['title'],
+            through: Vote,
+            as: 'voted_posts'
+          }
+        ]
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
         }
         res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log (err);
+      })
+      .catch(err => {
+        console.log(err);
         res.status(500).json(err);
-    });
+      });
 });
 
 //POST /api/users
