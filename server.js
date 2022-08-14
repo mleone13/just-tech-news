e702a899cc986d8d4bb98276ebeade352deb3502
Happy Gilmore <maryleone@rocketcentral.com>
2022-08-13 22:15:38 -0400

    javascript

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+const session = require('express-session');
+
+const SequelizeStore=require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
  
@@ -17,6 +31,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
